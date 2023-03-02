14c4e1dc5c7a7d12054d892d21102960d11ef1cd
Tetiana Koltunova <qaengineer.tetiana.koltunova@gmail.com>
2023-03-02 16:44:59 +0200

    updated CamerasTest

--- src/test/java/ui/CameraTest.java
+++ src/test/java/ui/CameraTest.java
@@ -1,5 +1,8 @@
 package ui;
 
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.have;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
 import com.codeborne.selenide.CollectionCondition;
@@ -26,16 +29,19 @@ public class CameraTest extends BaseTest {
 
     String cameraNameToCheck = "Canon EOS 5D";
     //Check that Canon EOS 5D has old price 122.00
-    camerasList.filterBy(Condition.ownText(cameraNameToCheck)).get(0)
-        .shouldHave(Condition.attribute("class","price-old").text("122.00"));
+    camerasList.filterBy(have(text(cameraNameToCheck))).get(0)
+        .shouldHave(attribute("class", "price-old"))
+        .shouldHave(text("122.00"));
 
     //Check that Canon EOS 5D has new price 98.00
     camerasList.filterBy(Condition.ownText(cameraNameToCheck)).get(0)
-        .shouldHave(Condition.attribute("class","price-new").text("98.00"));
+        .shouldHave(attribute("class", "price-new"))
+        .shouldHave(text("98.00"));
 
     //Check that Nikon D300 has ex.rate 80.00
     cameraNameToCheck = "Nikon D300";
     camerasList.filterBy(Condition.exactOwnText(cameraNameToCheck)).get(0)
-        .shouldHave(Condition.attribute("class","price-tax").text("80.00"));
+        .shouldHave(attribute("class", "price-tax"))
+        .shouldHave(text("80.00"));
   }
 }
